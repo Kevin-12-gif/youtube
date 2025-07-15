eb1c96e3385ca342b96c53952a0dd707bacaf6de
Kevin-12-gif <kevinalimohsin@gmail.com>
2025-07-15 08:49:18 -0700

    Added gameUrl string

--- app/(tabs)/types.ts
+++ app/(tabs)/types.ts
@@ -3,5 +3,8 @@ export type RootStackParamList = {
   Home: undefined;
   Playlist: { title: string; playlistId: string };
   Video: { videoId: string; title: string };
+  Game: { gameUrl: string }; 
 };
+
+
  
