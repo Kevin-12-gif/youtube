b5e0f1413a3af2f391b0af47c47f6cc6f10bccca
Kevin-12-gif <kevinalimohsin@gmail.com>
2025-08-30 13:43:38 -0700

    modified:   app/(tabs)/HomeScreen.tsx 	modified:   app/(tabs)/types.ts

--- app/(tabs)/types.ts
+++ app/(tabs)/types.ts
@@ -2,6 +2,11 @@
 export type RootStackParamList = {
   Home: undefined;
   Playlist: { title: string; playlistId: string };
-  Video: { videoId: string; title: string }; // These are required
-  Game: { gameUrl: string };
+  Video: { videoId: string };
+  Neon2048: undefined;
+  NeonPong: undefined;
+  NeonSnake: undefined;
+  NeuralSync: undefined;
+  ColorCrush: undefined;
+  BugSwatDefense: undefined;
 };
